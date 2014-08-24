c652a3a5df50cdde2b0f1d8092c8a8788ef30363
Aaron Chou <aaron@ecrowdmedia.com>
2014-08-24 23:29:00 +0800

    first test version of Reader

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,22 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'MooReader_site' });
-};
+
+module.exports = function(app){
+	// http://localhost:9000/reader.html?cid=moby-dick#p=0
+	app.get('/:name', function(req, res){
+		// res.render('index', {title: 'MooReader_site'});
+		// console.log(__dirname + '/../public/reader.html');
+		console.log('name: ' + req.params.name);
+		if(req.params.name === 'manifest.appcache'){
+			res.sendfile('manifest.appcache', {root: './public'});
+		} else{
+			res.sendfile('reader.html', {root: './public'});
+		}
+	});
+
+	app.get('/plugins/:', function(req, res){
+		console.log('got plugins request!');
+	});
+
+}
